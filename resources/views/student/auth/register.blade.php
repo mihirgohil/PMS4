1a0b0ea75c25eb9b89823f00f0283621198266f0
rushimhaske18 <mrrushikesh99@gmail.com>
2020-11-03 18:20:59 +0530

    register

--- resources/views/student/auth/register.blade.php
+++ resources/views/student/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Student Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('student.register') }}" aria-label="{{ __('Register') }}">
