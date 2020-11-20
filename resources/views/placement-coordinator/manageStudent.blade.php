ed17b3786c1e6f1e82fe26a7b160c3583c2c400e
rushimhaske18 <mrrushikesh99@gmail.com>
2020-11-21 03:51:41 +0530

    formatting

--- resources/views/placement-coordinator/manageStudent.blade.php
+++ resources/views/placement-coordinator/manageStudent.blade.php
@@ -19,21 +19,21 @@
         <div class="row"> 
               @foreach($student as $data)
                 <div class="col-sm-4"> 
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 22rem;">
                   <img class="card-img-top" src="{{$data->avatar}}" alt="Card image cap">
                   <div class="card-body">
-                    <h5 class="card-title">Student name : <br> {{$data->studentname}}</h5>
-                    <p class="card-text">{{$data->enrollment_no}}</a></p>
-                    <p class="card-text">{{$data->phone}}</p>
-                    <p class="card-text">{{$data->email}}</p>
-                    <p class="card-text">{{$data->dob}}</p>
-                    <p class="card-text">{{$data->drive}}</p>
-                    <p class="card-text">{{$data->ssc}}</p>
-                    <p class="card-text">{{$data->hsc}}</p>
-                    <p class="card-text">{{$data->ug}}</p>
-                    <p class="card-text">{{$data->stream}}</p>
-                    <p class="card-text">{{$data->cpi}}</p>
-                    <p class="card-text">{{$data->created_at}}</p>
+                    <h5 class="card-title">Student name :<br>{{$data->studentname}}</h5><br><br>
+                    <p class="card-text">Enrollment No : {{$data->enrollment_no}}</a></p>
+                    <p class="card-text">Phone No.: {{$data->phone}}</p>
+                    <p class="card-text">Email : {{$data->email}}</p>
+                    <p class="card-text">Date Of Birth : {{$data->dob}}</p>
+                    <!-- <p class="card-text">Placement Drive : {{$data->drive}}</p> -->
+                    <p class="card-text">S.S.C(%) : {{$data->ssc}}</p>
+                    <p class="card-text">H.S.C(%) : {{$data->hsc}}</p>
+                    <p class="card-text">U.G(%) : {{$data->ug}}</p>
+                    <p class="card-text">Stream : {{$data->stream}}</p>
+                    <p class="card-text">CPI : {{$data->cpi}}</p>
+                    <p class="card-text">Created At : {{$data->created_at}}</p>
                     <a href="{{ route('placement-coordinator.editStudentSelect', ['id' => $data->id ]) }}" class="btn btn-primary">Edit</a>
                   </div>
                 </div>
