d22bd1ab9e9e41b7509e5adc13ef59fe4b670fd9
rushimhaske18 <mrrushikesh99@gmail.com>
2020-12-15 17:57:39 +0530

    student login page log remove

--- resources/views/student/layouts/app.blade.php
+++ resources/views/student/layouts/app.blade.php
@@ -27,7 +27,7 @@
     <nav class="navbar navbar-expand-md navbar-dark bg-primary navbar-laravel">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img class="logo" src="{{ asset('images/college_logo.png')}}">
+                <!-- <img class="logo" src="{{ asset('images/college_logo.png')}}"> -->
               {{ __('PMS:: Student Portal ')}}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
